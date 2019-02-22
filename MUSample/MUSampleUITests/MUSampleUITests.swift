231c99404f473c5661609d006a58e7a8bd2e3c53
Loïc GRIFFIE <lgriffie@mbp-de-loic.lan>
2019-02-22 16:24:23 +0100

    Fix UITests scheme

--- MUSample/MUSampleUITests/MUSampleUITests.swift
+++ MUSample/MUSampleUITests/MUSampleUITests.swift
@@ -2,7 +2,7 @@
 //  MUSampleUITests.swift
 //  MUSampleUITests
 //
-//  Created by Damien Noël Dubuisson on 21/02/2019.
+//  Created by Loïc GRIFFIE on 22/02/2019.
 //  Copyright © 2019 Damien Noël Dubuisson. All rights reserved.
 //
 
