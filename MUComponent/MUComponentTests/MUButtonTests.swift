56dc2ea887859dd04e3320b6c70e6284131c3b53
Damien Noël Dubuisson <damien@rlb-mbp-01.lan>
2019-02-25 16:57:51 +0100

    Add MUHeader

--- MUComponent/MUComponentTests/MUButtonTests.swift
+++ MUComponent/MUComponentTests/MUButtonTests.swift
@@ -1,6 +1,6 @@
 //
 //  MUButtonTests.swift
-//  MUButtonTests
+//  MUComponentTests
 //
 //  Created by Damien Noël Dubuisson on 21/02/2019.
 //  Copyright © 2019 Damien Noël Dubuisson. All rights reserved.
