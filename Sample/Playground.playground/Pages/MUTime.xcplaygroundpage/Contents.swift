d13cb667ab81f22a5ceaf2a05cbfa3403c2df60b
Loïc GRIFFIE <lgriffie@MacBook-Pro-de-Loic.local>
2019-03-20 11:23:12 +0100

    Migrate MUTime

---
+++ Sample/Playground.playground/Pages/MUTime.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+
+import UIKit
+import PlaygroundSupport
+import Sejima
+
+// ### Global View ###
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+view.backgroundColor = .black
+
+PlaygroundPage.current.liveView = view
+
+let time = MUTime()
+time.backgroundColor = UIColor.white
+time.indicatorMinValue = 0
+time.indicatorMaxValue = 120
+time.indicatorStartAngle = 270
+time.indicatorEndAngle = 90
+time.animationDuration = 0.75
+time.indicatorLineCap = .round
+time.timeColor = .white
+time.timeBackgroundColor = .green
+time.indicatorColor = .orange
+time.indicatorWidth = 2
+time.format = "%.f'"
+
+view.addSubview(time)
+time.centerXAnchor
+    .constraint(equalTo: view.centerXAnchor)
+    .isActive = true
+time.centerYAnchor
+    .constraint(equalTo: view.centerYAnchor)
+    .isActive = true
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+    time.set(value: 90, animated: true, progress: { current in
+        print(current)
+    })
+}
+
+//: [Next](@next)
---
+++ Sejima/SejimaTests/MUTimeTests.swift
@@ -0,0 +1,64 @@
+//
+//  MUTimeTests.swift
+//  Tests
+//
+//  Created by Loïc GRIFFIE on 31/10/2018.
+//  Copyright © 2018 Loïc GRIFFIE. All rights reserved.
+//
+
+import XCTest
+@testable import Sejima
+
+class MUTimeTests: XCTestCase {
+
+    func testDefaultValues() {
+        let time = MUTime()
+        XCTAssertNotNil(time)
+
+        XCTAssertEqual(time.indicatorMinValue, 0)
+        XCTAssertEqual(time.indicatorMaxValue, 90)
+        XCTAssertEqual(time.indicatorStartAngle, 0)
+        XCTAssertEqual(time.indicatorEndAngle, 360)
+        XCTAssertEqual(time.animationDuration, 0.3)
+        XCTAssertEqual(time.indicatorLineCap, .round)
+        XCTAssertEqual(time.timeColor, .clear)
+        XCTAssertEqual(time.timeBackgroundColor, .clear)
+        XCTAssertEqual(time.indicatorColor, .clear)
+        XCTAssertEqual(time.indicatorWidth, 8)
+        XCTAssertEqual(time.format, "%.f")
+        XCTAssertEqual(time.showValue, true)
+    }
+
+    func testCustomValues() {
+        let time = MUTime()
+        XCTAssertNotNil(time)
+
+        time.backgroundColor = .white
+        time.indicatorMinValue = 0
+        time.indicatorMaxValue = 120
+        time.indicatorStartAngle = 270
+        time.indicatorEndAngle = 90
+        time.animationDuration = 0.75
+        time.indicatorLineCap = .round
+        time.timeColor = .black
+        time.timeBackgroundColor = .orange
+        time.indicatorColor = .green
+        time.indicatorWidth = 2
+        time.format = "%.f%%"
+        time.showValue = false
+
+        XCTAssertEqual(time.backgroundColor, .white)
+        XCTAssertEqual(time.indicatorMinValue, 0)
+        XCTAssertEqual(time.indicatorMaxValue, 120)
+        XCTAssertEqual(time.indicatorStartAngle, 270)
+        XCTAssertEqual(time.indicatorEndAngle, 90)
+        XCTAssertEqual(time.animationDuration, 0.75)
+        XCTAssertEqual(time.indicatorLineCap, .round)
+        XCTAssertEqual(time.timeColor, .black)
+        XCTAssertEqual(time.timeBackgroundColor, .orange)
+        XCTAssertEqual(time.indicatorColor, .green)
+        XCTAssertEqual(time.indicatorWidth, 2)
+        XCTAssertEqual(time.format, "%.f%%")
+        XCTAssertEqual(time.showValue, false)
+    }
+}
