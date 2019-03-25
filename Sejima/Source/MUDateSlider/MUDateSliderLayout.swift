b5f7eaaf83b3006f9c4f1d26f9d119debc5cb760
damien-nd <damien-nd@users.noreply.github.com>
2019-03-25 15:54:29 +0100

    Update MUDateSliderLayout.swift

--- Sejima/Source/MUDateSlider/MUDateSliderLayout.swift
+++ Sejima/Source/MUDateSlider/MUDateSliderLayout.swift
@@ -2,8 +2,10 @@
 //  MUDateSliderLayout.swift
 //  Sejima
 //
-//  Created by Huong Do on 9/27/16.
-//  Copyright © 2016 ichigo. All rights reserved.
+//  Created by Loïc GRIFFIE on 31/10/2018.
+//  Copyright © 2018 Loïc GRIFFIE. All rights reserved.
+//
+//  Based on: https://github.com/itsmeichigo/DateTimePicker
 //
 
 import UIKit
