1e4da20de29fd9d96100dd0182689a393104fda7
Damien Noël Dubuisson <damien@rlb-mbp-01.lan>
2019-03-28 17:42:52 +0100

    Improve documentation

--- Sejima/Source/MUCore/MUMatrix.swift
+++ Sejima/Source/MUCore/MUMatrix.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
+    /// 2D array of matrix datas.
     public private(set) var datas: [[T]]
+    /// Number of rows.
     public private(set) var rowsCount: Int
+    /// Number of columns.
     public private(set) var columnsCount: Int
 
     private init() {
@@ -19,12 +22,14 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         columnsCount = 0
     }
 
+    /// Initialize a matrix with a single value repeated on each rows and columns.
     public init(rows: Int, columns: Int, repeatedValue: T) {
         datas = [[T]](repeating: [T](repeating: repeatedValue, count: columns), count: rows)
         rowsCount = rows
         columnsCount = columns
     }
 
+    /// Accesses the element at the specified position.
     public subscript(row: Int, column: Int) -> T {
         get {
             checkRow(row)
@@ -39,42 +44,21 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         }
     }
 
-    // MARK: -
+    // MARK: - Public functions
 
-    private func checkRow(_ index: Int) {
-        precondition(index >= 0 && index < rowsCount, "Row index out of bounds")
-    }
-
-    private func checkColumn(_ index: Int) {
-        precondition(index >= 0 && index < columnsCount, "Column index out of bounds")
-    }
-
-    // MARK: -
-
+    /// Get the row at the specified index.
     public func row(_ index: Int) -> [T] {
         checkRow(index)
         return datas[index]
     }
 
+    /// Get the column at the specified index.
     public func column(_ index: Int) -> [T] {
         checkColumn(index)
         return datas.map { $0[index] }
     }
 
-    public func rows() -> [[T]] {
-        return datas
-    }
-
-    public func columns() -> [[T]] {
-        return datas.reduce([[T]](repeating: [T](), count: columnsCount), { arr, rows in
-            var array = arr
-            (0 ..< columnsCount).forEach { i in
-                array[i].append(rows[i])
-            }
-            return array
-        })
-    }
-
+    /// Flips the matrix over its diagonal.
     public func transpose() -> MUMatrix {
         var transposed = MUMatrix()
         transposed.rowsCount = columnsCount
@@ -83,6 +67,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         return transposed
     }
 
+    /// Resize the matrix and crop values if new matrix is smaller or add 0 if new matrix is bigger.
     public func resize(row: Int, column: Int) -> MUMatrix {
         var resultMatrix = MUMatrix(rows: row, columns: column, repeatedValue: T(0))
         var flatDatas = datas.flatMap { $0 }
@@ -100,52 +85,12 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         return resultMatrix
     }
 
-    // This method is mostly untested. It has been included for the cases where the
-    // `singleValueBackSubstitution` method has a b column count greater than one,
-    // but this does not occur with perspective correction (our curent use case) and
-    // therefore I have not been able to test it thoroughly. I've included it here
-    // for completeness.
-    //
-    // y[0:m,0:n] += diag(a[0:1,0:m]) * x[0:m,0:n]
-    public func matrixAXPY(row: Int, column: Int, aMatrix: MUMatrix, yMatrix: inout MUMatrix) {
-        let xRowsCount = rowsCount
-        let yRowsCount = yMatrix.rowsCount
-
-        (0 ..< row).forEach { rowIndex in
-            let aValue = aMatrix[rowIndex, 0]
-            var xColumnIndex = 0
-            var yColumnIndex = 0
-
-            (0 ..< column).forEach { _ in
-                // Account for different Matrix dimensions
-                let xColWrap = rowIndex / xRowsCount
-                if xColWrap >= 1 {
-                    xColumnIndex += xColWrap
-                }
-
-                let yColWrap = rowIndex / yRowsCount
-                if yColWrap >= 1 {
-                    yColumnIndex += yColWrap
-                }
-
-                // xMatrix should not change in this method make sure we have enough xMatrix columns
-                if xColumnIndex < columnsCount {
-                    let yValue = yMatrix[(rowIndex % yRowsCount), yColumnIndex]
-                    let xValue = self[(rowIndex % xRowsCount), xColumnIndex]
-                    yMatrix[(rowIndex % yRowsCount), yColumnIndex] = yValue + aValue * xValue
-                }
-
-                xColumnIndex += 1
-                yColumnIndex += 1
-            }
-        }
-    }
-
-    func singleValueBackSubstitution(row: Int,
-                                     column: Int,
-                                     wMatrix: MUMatrix,
-                                     vMatrix: MUMatrix?,
-                                     bMatrix: MUMatrix?) -> MUMatrix {
+    /// Jacobi Single Value Backward Substitution.
+    public func singleValueBackSubstitution(row: Int,
+                                            column: Int,
+                                            wMatrix: MUMatrix,
+                                            vMatrix: MUMatrix?,
+                                            bMatrix: MUMatrix?) -> MUMatrix {
         let bCols = bMatrix?.columnsCount ?? column
         var xMatrix = MUMatrix(rows: row, columns: bCols, repeatedValue: T(0))
         var threshold: T = 0
@@ -201,13 +146,80 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         return xMatrix
     }
 
+    // MARK: - Private functions
+
+    private func checkRow(_ index: Int) {
+        precondition(index >= 0 && index < rowsCount, "Row index out of bounds")
+    }
+
+    private func checkColumn(_ index: Int) {
+        precondition(index >= 0 && index < columnsCount, "Column index out of bounds")
+    }
+
+    private func rows() -> [[T]] {
+        return datas
+    }
+
+    private func columns() -> [[T]] {
+        return datas.reduce([[T]](repeating: [T](), count: columnsCount), { arr, rows in
+            var array = arr
+            (0 ..< columnsCount).forEach { i in
+                array[i].append(rows[i])
+            }
+            return array
+        })
+    }
+
+    // This method is mostly untested. It has been included for the cases where the
+    // `singleValueBackSubstitution` method has a b column count greater than one,
+    // but this does not occur with perspective correction (our curent use case) and
+    // therefore I have not been able to test it thoroughly. I've included it here
+    // for completeness.
+    //
+    // y[0:m,0:n] += diag(a[0:1,0:m]) * x[0:m,0:n]
+    private func matrixAXPY(row: Int, column: Int, aMatrix: MUMatrix, yMatrix: inout MUMatrix) {
+        let xRowsCount = rowsCount
+        let yRowsCount = yMatrix.rowsCount
+
+        (0 ..< row).forEach { rowIndex in
+            let aValue = aMatrix[rowIndex, 0]
+            var xColumnIndex = 0
+            var yColumnIndex = 0
+
+            (0 ..< column).forEach { _ in
+                // Account for different Matrix dimensions
+                let xColWrap = rowIndex / xRowsCount
+                if xColWrap >= 1 {
+                    xColumnIndex += xColWrap
+                }
+
+                let yColWrap = rowIndex / yRowsCount
+                if yColWrap >= 1 {
+                    yColumnIndex += yColWrap
+                }
+
+                // xMatrix should not change in this method make sure we have enough xMatrix columns
+                if xColumnIndex < columnsCount {
+                    let yValue = yMatrix[(rowIndex % yRowsCount), yColumnIndex]
+                    let xValue = self[(rowIndex % xRowsCount), xColumnIndex]
+                    yMatrix[(rowIndex % yRowsCount), yColumnIndex] = yValue + aValue * xValue
+                }
+
+                xColumnIndex += 1
+                yColumnIndex += 1
+            }
+        }
+    }
+
     // MARK: - Mutating functions
 
+    /// Fill the selected row at index with a repeated value.
     public mutating func fillRow(_ index: Int, value: T) {
         checkRow(index)
         datas[index] = [T](repeating: value, count: columnsCount)
     }
 
+    /// Fill the selected column at index with a repeated value.
     public mutating func fillColumn(_ index: Int, value: T) {
         checkColumn(index)
         (0 ..< rowsCount).forEach { i in
@@ -215,6 +227,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         }
     }
 
+    /// Insert a new row at selected index and fill it with a repeated value.
     @discardableResult
     public mutating func insertRow(_ index: Int, value: T) -> MUMatrix {
         checkRow(index)
@@ -223,6 +236,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         return self
     }
 
+    /// Insert a new column at selected index and fill it with a repeated value.
     @discardableResult
     public mutating func insertColumn(_ index: Int, value: T) -> MUMatrix {
         checkColumn(index)
@@ -233,6 +247,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         return self
     }
 
+    /// Perform a rotation in Euclidean space.
     public mutating func rotate(firstRow: Int, secondRow: Int, cosine: T, sine: T) {
         (0 ..< columnsCount).forEach { index in
             let t0 = self[firstRow, index] * cosine + self[secondRow, index] * sine
@@ -242,6 +257,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
         }
     }
 
+    /// Swap first and second value at specified row/column.
     public mutating func swapValue(first: (row: Int, column: Int), second: (row: Int, column: Int)) {
         let temp = self[first.row, first.column]
         self[first.row, first.column] = self[second.row, second.column]
@@ -250,6 +266,7 @@ public struct MUMatrix<T: MUFloatingPoint & CustomStringConvertible> {
 }
 
 extension MUMatrix: CustomStringConvertible {
+    /// A textual representation of this instance.
     public var description: String {
         var text = "[\n"
         (0 ..< rowsCount).forEach { row in
@@ -264,8 +281,10 @@ extension MUMatrix: CustomStringConvertible {
 }
 
 extension MUMatrix: ExpressibleByArrayLiteral {
+    /// The type of the elements of an array literal.
     public typealias ArrayLiteralElement = [T]
 
+    /// Creates an instance initialized with the given elements.
     public init(arrayLiteral elements: [T]...) {
         datas = elements
         rowsCount = elements.count
