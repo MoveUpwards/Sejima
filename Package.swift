37a7d6011596a4cb90d33a247733b1273791f988
lgriffie <loic@griffie.fr>
2021-01-19 11:00:03 +0100

    Update Neumann

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/MoveUpwards/Neumann.git", from: "1.0.0")
+        .package(url: "https://github.com/MoveUpwards/Neumann.git", from: "2.0.0")
     ],
     targets: [
         .target(
