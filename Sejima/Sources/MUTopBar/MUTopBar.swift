f47639ad2d30ae4a8a422cfed84ab033e5b7cfee
lgriffie <loic@griffie.fr>
2021-01-20 16:20:13 +0100

    Add MUTopBar height property

--- Sejima/Sources/MUTopBar/MUTopBar.swift
+++ Sejima/Sources/MUTopBar/MUTopBar.swift
@@ -110,6 +110,9 @@ open class MUTopBar: MUNibView {
         }
     }
 
+    /// Describes the MUTopBar's height while it shows
+    @IBInspectable open dynamic var height: CGFloat = 65.0
+
     // MARK: - Private functions
 
     private func updateImageWidth() {
@@ -132,8 +135,22 @@ open class MUTopBar: MUNibView {
         delegate?.didTap(topBar: self)
     }
 
+    override open func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let window = UIApplication.shared.keyWindow else { return super.sizeThatFits(size) }
+
+        var sizeThatFits = super.sizeThatFits(size)
+        if height > 0.0 {
+            if #available(iOS 11.0, *) {
+                sizeThatFits.height = height + window.safeAreaInsets.top
+            } else {
+                sizeThatFits.height = height
+            }
+        }
+        return sizeThatFits
+    }
+
     /// The natural size for the receiving view, considering only properties of the view itself.
     override open var intrinsicContentSize: CGSize {
-        return CGSize(width: 375, height: 44) // To act like a UINavigationBar with a default size
+        return sizeThatFits(.zero)
     }
 }
